85940495b1d31f1088212f3604d926578b7475c6
Nurdin kurniawan <nurdink16@gmail.com>
2023-05-11 16:50:15 +0700

    uts

--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,30 @@
 package pengguna;
 
 public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 
 }
